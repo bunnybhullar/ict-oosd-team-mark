2e40a78a5d5827c99c8ea7d767572ea69550d73a
dumindagunasekara <dumindagunasekara@308066ee-9ee9-11de-a8ff-c955b97a4a10>
2009-09-14 17:37:10 +0000

    Package GUI done

--- travel-experts/src/ca/sait/oosd/util/ArgumentAssertionChecker.java
+++ travel-experts/src/ca/sait/oosd/util/ArgumentAssertionChecker.java
@@ -6,11 +6,21 @@
 
 package ca.sait.oosd.util;
 
+import java.util.Date;
+
 public class ArgumentAssertionChecker {
 
 	public static void assertNotNull(Object object) throws TENullValueException {
 		if(object == null) {
 			throw new TENullValueException("The value of the obejct could not be empty");
 		}
+	}
+	
+	public static void assertNotEmpty(Object object) {
+		
+	}
+	
+	public static void assertValidDate(Date date) {
+		
 	}
 }
