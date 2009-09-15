477743388e5188788a41dbada37a42de38a66d51
dumindagunasekara <dumindagunasekara@308066ee-9ee9-11de-a8ff-c955b97a4a10>
2009-09-15 04:15:03 +0000

    Agents and Products GUIs

--- trunk/travel-experts-view/src/ca/sait/oosd/travel/products/ProductsGUI.java
+++ trunk/travel-experts-view/src/ca/sait/oosd/travel/products/ProductsGUI.java
@@ -9,17 +9,22 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.SpringLayout;
 
+import ca.sait.oosd.SpringUtilities;
 import ca.sait.oosd.TravelParts;
 import ca.sait.oosd.business.TEBusinessDelegate;
 import ca.sait.oosd.business.TEBusinessDelegateImpl;
 import ca.sait.oosd.business.TEBusinessException;
 import ca.sait.oosd.components.NavigationButtonPanel;
 import ca.sait.oosd.components.TEJFrame;
+import ca.sait.oosd.components.Validator;
+import ca.sait.oosd.components.ValidatorException;
 import ca.sait.oosd.logger.LogLevel;
 import ca.sait.oosd.logger.LoggerHelper;
 import ca.sait.oosd.hibernate.Products;
@@ -37,50 +42,59 @@ public class ProductsGUI extends TEJFrame {
 
 	private static final long serialVersionUID = 1L;
 	private LoggerHelper helper = new LoggerHelper(ProductsGUI.class.getName());
+
 	Collection<Products> productCollection;
-    private TEBusinessDelegate delegate;
-    private Products products;
-    private DefaultListModel model;
-    
+	private TEBusinessDelegate delegate;
+	private Products products;
+	private DefaultListModel model;
 	private JTextField productIdTextField = new JTextField(20);
 	private JTextField productNameTextField = new JTextField(20);
 
-    private final int WIDTH = 600;
-	private final int HEIGHT = 500;
+
+    private final int WIDTH = 850;
+	private final int HEIGHT = 300;
 	
 	public ProductsGUI() {
 		super();
-		
-		//this should be the way to call delegate. Since this is not implemented yet, we can use some dummy data as below
+
 		delegate = new TEBusinessDelegateImpl();
 		productCollection = delegate.getProductsCollection();
 
-        model = new DefaultListModel();
-        
-		this.initGUI();
-        this.adjustSize(WIDTH, HEIGHT);
-        this.alignFrameOnScreen(WIDTH, HEIGHT);
+		model = new DefaultListModel();
 		
-	}
-	
-	@Override
-	protected void adjustSize(int width, int height) {
-        this.setSize(new Dimension(width, height));
+		this.initGUI();
+		this.adjustSize(WIDTH, HEIGHT);
+		super.alignFrameOnScreen(WIDTH, HEIGHT);
+
 	}
 
 	@Override
 	protected void initGUI() {
+		helper.log(LogLevel.INFO,
+		"Start initlializing Products user interface...");
+
 		this.setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		JPanel dataEntryPane = new JPanel(new GridLayout(2, 2));
+		JPanel dataEntryPane = new JPanel(new SpringLayout());
 		dataEntryPane.add(new JLabel("Product ID", JLabel.TRAILING));
+		productIdTextField.setEnabled(false);
 		dataEntryPane.add(productIdTextField);
 		dataEntryPane.add(new JLabel("Product Name", JLabel.TRAILING));
 		dataEntryPane.add(productNameTextField);
+		
+        SpringUtilities.makeCompactGrid(dataEntryPane,
+				2, 2,
+				6, 6,
+				6, 6);		
+        
+		GridLayout grid = new GridLayout(2, 1);
+		JPanel centerPane = new JPanel();
+		centerPane.setLayout(grid);
 
+		centerPane.add(dataEntryPane);        
 
-		//list to display the data
+		// list to display the data
 		final JList list = new JList(productCollection.toArray());
 		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		list.setLayoutOrientation(JList.VERTICAL);
@@ -89,7 +103,7 @@ public class ProductsGUI extends TEJFrame {
 		JScrollPane listScroller = new JScrollPane(list);
 		listScroller.setPreferredSize(new Dimension(250, 80));
 
-		//button panel
+		// button panel
 		JPanel southPane = new JPanel();
 		JButton addButton = new JButton("Add");
 		JButton updateButton = new JButton("Update");
@@ -100,158 +114,145 @@ public class ProductsGUI extends TEJFrame {
 		southPane.add(clearButton);
 		southPane.add(deleteButton);
 
-		JPanel northPane = new JPanel();
-		JButton productButton = new JButton("Product");
-		JButton supplierButton = new JButton("Supplier");
-		JButton packageButton = new JButton("Package");
-		JButton psupplierButton = new JButton("Product Supplier");
-		JButton ppsupplierButton = new JButton("Package Product Supplier");
-		JButton mmenuButton = new JButton("Main Menu");
-		northPane.add(productButton);
-		northPane.add(supplierButton);
-		northPane.add(packageButton);
-		northPane.add(psupplierButton);
-		northPane.add(ppsupplierButton);
-		northPane.add(mmenuButton);
+		this.getContentPane().add(new NavigationButtonPanel(TravelParts.PRODUCT), BorderLayout.NORTH);
+		this.getContentPane().add(centerPane, BorderLayout.CENTER);
+		this.getContentPane().add(listScroller, BorderLayout.EAST);
+		this.getContentPane().add(southPane, BorderLayout.SOUTH);
 
-
-
-		GridLayout grid = new GridLayout(2, 1);
-		JPanel centerPane = new JPanel();
-		centerPane.setLayout(grid);
-		//centerPane.add(northPane, JPanel.TOP_ALIGNMENT);
-		centerPane.add(dataEntryPane);
-		centerPane.add(southPane, JPanel.BOTTOM_ALIGNMENT);
-
-		JPanel mainPane = new JPanel(new BorderLayout());
-		mainPane.add(new NavigationButtonPanel(TravelParts.PRODUCT), BorderLayout.NORTH);
-		mainPane.add(centerPane, BorderLayout.CENTER);
-		mainPane.add(listScroller, BorderLayout.EAST);
-
-		this.getContentPane().add(mainPane, BorderLayout.CENTER);
-
-		//add the listener to the list
+		// add the listener to the list
 		ListSelectionModel selectionModel = list.getSelectionModel();
 		selectionModel.addListSelectionListener(new ListSelectionListener() {
 
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
-				//TODO Auto-generated method stub
-				 products = (Products)list.getSelectedValue();
-				productIdTextField.setText(Long.toString(products.getProductid()));
+				products = (Products) list.getSelectedValue();
+				productIdTextField.setText(Long.toString(products
+						.getProductid()));
 				productNameTextField.setText(products.getProdname());
+				productIdTextField.setEnabled(false);
 			}
 
 		});
 
-		//Clear the content of the data entry
+		// Clear the content of the data entry
 		clearButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-					clearComponents();
+				clearComponents();
 			}
 		});
-
-		//Add new records to the database
-		addButton.addActionListener(new ActionListener()
-		{
+		// Add new records to the database
+		addButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				try{
+				try {
+					validateForm();
+
 					products = new Products();
 					products.setProdname(productNameTextField.getText().trim());
 
-                    products = (Products)delegate.save(products);
-
-                    model.addElement(products);
+					products = (Products) delegate.save(products);
+					model.addElement(products);
 					clearComponents();
-                    
-				}catch (TEBusinessException ex){
-					helper.log(LogLevel.ERROR, "Exception occured while saving Data.class.." + ex.getMessage());
-				}
 
+				} catch (TEBusinessException ex) {				
+					helper.log(LogLevel.ERROR,
+							"Exception occured while saving Data..."
+							+ ex.getMessage());
+
+				} catch (ValidatorException ex) {
+					helper.log(LogLevel.ERROR, "Validation exception occured..." + ex.getMessage());
+					JOptionPane.showMessageDialog(ProductsGUI.this, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
+				}		
 			}
 
 		});
-		//delete records from the database
-		deleteButton.addActionListener(new ActionListener()
-		{
+		// delete records from the database
+		deleteButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				if(products != null){
-					try{
+				if (products != null) {
+					try {
 						int position = model.indexOf(products, 0);
+						delegate.delete(products);
 
-                        delegate.delete(products);
-
-                        clearComponents();
+						clearComponents();
 						removeFromListModel(position);
-                        
-					}catch (TEBusinessException ex){
-						helper.log(LogLevel.ERROR, "Exception occured while saving Data.class.." + ex.getMessage());
+
+					} catch (TEBusinessException ex) {
+						helper.log(LogLevel.ERROR,
+								"Exception occured while saving Data.class.."
+								+ ex.getMessage());
 					}
 				}
 			}
 
-			private void removeFromListModel(int position) {
-				// TODO Auto-generated method stub
-
-			}
-
-		});
-		//update records from the user interface
-		updateButton.addActionListener(new ActionListener()
-		{
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				try{
-				products = new Products();
-				products.setProductid(Long.parseLong(productIdTextField.getText()));
-				products.setProdname(productNameTextField.getText().trim());
-
-                products = (Products)delegate.update(products);
-                
-                updateListModel(products);
-                
-				} catch (TEBusinessException e1) {
-					// TODO Auto-generated catch block
-					helper.log(LogLevel.ERROR, "Exception occured while saving Data.class.." + e1.getMessage());
-				}
-			}
 
 		});
 		
-	}
+		// update records from the user interface
+		updateButton.addActionListener(new ActionListener() {
 
-	//update the list model when user update a record from the user interface
-	protected void updateListModel(Products products2) {
-		// TODO Auto-generated method stub
-		for(Products prod : productCollection){
-			if(prod.getProductid() == products.getProductid()){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					validateForm();
+
+					products = new Products();
+					products.setProductid(Long.parseLong(productIdTextField.getText()));
+					products.setProdname(productNameTextField.getText().trim());
+					// try {
+					products = (Products) delegate.update(products);
+					updateListModel(products);
+
+				} catch (TEBusinessException e1) {
+					// TODO Auto-generated catch block
+					helper.log(LogLevel.ERROR,
+							"Exception occured while saving Data.class.."
+							+ e1.getMessage());
+				} catch (ValidatorException ex) {
+					helper.log(LogLevel.ERROR, "Validation exception occured..." + ex.getMessage());
+					JOptionPane.showMessageDialog(ProductsGUI.this, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+
+	}
+	
+	// update the list model when user update a record from the user interface
+	protected void updateListModel(Products products) {
+		for (Products prod : productCollection) {
+			if (prod.getProductid() == products.getProductid()) {
 				int position = model.indexOf(prod, 0);
+
 				model.set(position, products);
 			}
 		}
 	}
 
 	protected void removeFromListModel(int position) {
-		// TODO Auto-generated method stub
 		model.set(position, products);
+		
 	}
 
 	protected void clearComponents() {
-		// TODO Auto-generated method stub
 		productIdTextField.setText("");
 		productNameTextField.setText("");
 
+	}
+	
+	private void validateForm() throws ValidatorException{
+		Validator.isEmptyString("Product Name", productNameTextField.getText());
+		
+	}
+	
+	@Override
+	protected void adjustSize(int width, int height) {
+        this.setSize(new Dimension(width, height));
 	}
 
 }
